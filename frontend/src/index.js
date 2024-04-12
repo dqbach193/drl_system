3e39ffb4a4d20177df4895995a1a7ccd3aee7498
Bach Quang <dqbach193@gmail.com>
2024-04-12 23:42:41 +0700

    add react auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { DRLContextProvider } from "./context/DRLContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DRLContextProvider>
-      <App />
-    </DRLContextProvider>
+    <AuthContextProvider>
+      <DRLContextProvider>
+        <App />
+      </DRLContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
