5adc4c23e776dc0f51562b5159bad03e30132f2d
Bach Quang <dqbach193@gmail.com>
2024-04-13 00:30:53 +0700

    add logout hook

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    const {logout} = useLogout();
+
+    const handleClick = () =>{
+        logout()
+    }
     return ( 
         <header>
             <div className="container">
@@ -8,6 +13,9 @@ const Navbar = () => {
                     <h1>Hệ thống điểm rèn luyện</h1>
                 </Link>
                 <nav>
+                    <div>
+                        <button onClick={handleClick}>Log out</button>
+                    </div>
                     <div>
                         <Link to='/login'>Đăng nhập</Link>
                         <Link to='/signup'>Đăng ký</Link>
