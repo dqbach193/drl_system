0318cd18f60b6294fbef40853e0277e05abe5f6e
Bach Quang <dqbach193@gmail.com>
2024-04-12 22:14:32 +0700

    add user model and routes on backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const diemRenLuyenRoutes = require('./routes/diemRenLuyen')
+const userRoutes = require('./routes/user')
 const mongoose = require('mongoose');
+
 
 //middleware
 app.use(express.json());
@@ -13,6 +15,7 @@ app.use((req, res, next)=>{
 
 //routes
 app.use('/drl', diemRenLuyenRoutes);
+app.use('/user', userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
